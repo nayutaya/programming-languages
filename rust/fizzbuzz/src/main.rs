f5a30951147661518d57a8d642b33737b1307e8a
Yuya Kato <yuyakato@gmail.com>
2019-06-04 20:07:01 +0900

    matchを使った実装を追加

--- rust/fizzbuzz/src/main.rs
+++ rust/fizzbuzz/src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     for i in 1..=20 {
+        /*
         if      i % 15 == 0 { println!("{}", "FizzBuzz"); }
         else if i %  5 == 0 { println!("{}", "Buzz"); }
         else if i %  3 == 0 { println!("{}", "Fizz"); }
         else { println!("{}", i); }
+        */
+        match i {
+            _ if i % 15 == 0 => { println!("{}", "FizzBuzz"); }
+            _ if i %  5 == 0 => { println!("{}", "Buzz"); }
+            _ if i %  3 == 0 => { println!("{}", "Fizz"); }
+            _ => { println!("{}", i); }
+        }
     }
 }
