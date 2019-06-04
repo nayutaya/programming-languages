1469b5ba55880b45be6c524200028ba75a7c5a38
Yuya Kato <yuyakato@gmail.com>
2019-06-04 20:01:52 +0900

    シンプルな実装を追加

--- rust/fizzbuzz/src/main.rs
+++ rust/fizzbuzz/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    for i in 1..=20 {
+        if      i % 15 == 0 { println!("{}", "FizzBuzz"); }
+        else if i %  5 == 0 { println!("{}", "Buzz"); }
+        else if i %  3 == 0 { println!("{}", "Fizz"); }
+        else { println!("{}", i); }
+    }
 }
