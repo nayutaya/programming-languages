562aa28d78b67d9c2f27ea048f24f55914c8b9df
Yuya Kato <yuyakato@gmail.com>
2019-06-04 20:09:52 +0900

    関数化した実装を追加

--- rust/fizzbuzz/src/main.rs
+++ rust/fizzbuzz/src/main.rs
@@ -1,3 +1,12 @@
+fn fizzbuzz1(i: u32) -> String {
+    match i {
+        _ if i % 15 == 0 => { "FizzBuzz".into() }
+        _ if i %  5 == 0 => { "Buzz".into() }
+        _ if i %  3 == 0 => { "Fizz".into() }
+        _ => { format!("{}", i) }
+    }
+}
+
 fn main() {
     for i in 1..=20 {
         /*
@@ -6,11 +15,14 @@ fn main() {
         else if i %  3 == 0 { println!("{}", "Fizz"); }
         else { println!("{}", i); }
         */
+        /*
         match i {
             _ if i % 15 == 0 => { println!("{}", "FizzBuzz"); }
             _ if i %  5 == 0 => { println!("{}", "Buzz"); }
             _ if i %  3 == 0 => { println!("{}", "Fizz"); }
             _ => { println!("{}", i); }
         }
+        */
+        println!("{}", fizzbuzz1(i));
     }
 }
