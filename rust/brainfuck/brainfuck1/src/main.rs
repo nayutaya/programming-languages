23218b276d5280c40c1685269d093c2e323573b1
Yuya Kato <yuyakato@gmail.com>
2019-06-04 20:43:53 +0900

    インクリメント命令の解析処理を追加

--- rust/brainfuck/brainfuck1/src/main.rs
+++ rust/brainfuck/brainfuck1/src/main.rs
@@ -1,8 +1,40 @@
+#[derive(Debug, PartialEq)]
+enum Instruction {
+    Increment,
+    Invalid,
+}
+
+fn parse_char(ch: char) -> Instruction {
+    match ch {
+        '>' => Instruction::Increment,
+        _   => Instruction::Invalid,
+    }
+}
+
+fn parse_instructions(inst: &str) -> Vec<Instruction> {
+    inst.chars().map(parse_char).collect()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn can_parse_char() {
+        assert_eq!(Instruction::Increment, parse_char('>'));
+        assert_eq!(Instruction::Invalid, parse_char(' '));
+    }
+
+    #[test]
+    fn can_parse_string() {
+        // assert_eq!(Vec::new(), parse_instructions(""));
+        assert!(parse_instructions("") == Vec::new());
+        assert_eq!(
+            vec![
+                Instruction::Increment,
+                Instruction::Invalid,
+            ],
+            parse_instructions("> "));
     }
 }
 
